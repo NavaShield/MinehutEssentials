17e56590ba23425cb23a64354276a148544f9198
Gangie <pleasedont@email.me>
2026-03-15 21:08:24 +0000

    Initial commit with mod structure + gitignore file (ill do the readme later)

---
+++ src/client/java/github/navashield/client/minehutessentialsClient.java
@@ -0,0 +1,10 @@
+package github.navashield.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class minehutessentialsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/github/navashield/minehutessentials.java
@@ -0,0 +1,10 @@
+package github.navashield;
+
+import net.fabricmc.api.ModInitializer;
+
+public class minehutessentials implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
